a8e2011f591218b9674033b06021a0c5c7c7f996
bussury <bussury.omary@gmail.com>
2022-04-01 12:18:26 +0300

    pack

--- entry/server.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from '../client.js'
-import { createRenderFunction } from 'fastify-vite-vue/server'
-import routes from '../routes.js'
-
-export default {
-  routes,
-  render: createRenderFunction(createApp),
-}
